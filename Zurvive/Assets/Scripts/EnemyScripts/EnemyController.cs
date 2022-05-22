48ca38840f911bd088b484d7e8e7a359d24a4e8b
Brandon Charles <brandonebcharles@gmail.com>
2022-05-21 20:59:00 -0700

    Enemy Controller Working

--- Zurvive/Assets/Scripts/EnemyScripts/EnemyController.cs
+++ Zurvive/Assets/Scripts/EnemyScripts/EnemyController.cs
@@ -35,7 +35,7 @@ public class EnemyController : HealthManager
     public void Movement()
     {
         Vector3 pos = transform.position;
-
+        
         Vector3 velocity = new Vector3(0, speed * Time.deltaTime, 0);
 
         pos += transform.rotation * velocity;
@@ -47,26 +47,27 @@ public class EnemyController : HealthManager
     {
         if(player == null)
         {
-            // Finds the players ship
-            target = GameObject.FindWithTag("Player").transform; 
-
+            //Find the player
+            target = GameObject.FindWithTag("Player").transform;
+            
             if(target != null)
             {
                 player = target.transform;
             }
         }
-
+        
         if(player == null)
         {
-            return; // Try again
+            return; //Try again
         }
 
-        Debug.Log(player.position);
         Vector3 dir = player.position - transform.position;
         dir.Normalize();
+
         float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
 
-        Quaternion desiredRot = Quaternion.Euler(0,0,zAngle);
+        Quaternion desiredRot = Quaternion.Euler(0, 0, zAngle);
+
         transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRot, rotationSpeed * Time.deltaTime);
     }
 
